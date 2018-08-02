341de34d5af02f6c585fef2b31451cb0cb75ced8
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-02 21:50:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,11 +6,12 @@ module FormsLab
     # code other routes/actions here
     get '/' do
       "Welcome to the Nested Forms Lab!"
+      "let's navigate to the '/new'"
       # erb :"pirates/new"
     end
 
     post '/pirates' do
-      
+
     erb :"pirates/show"
     end
 
